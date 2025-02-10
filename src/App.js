a159680ab47fa88a0fea07d7bcf69f12298ea0a4
agambrus <contato@ambrusplace.com.br>
2025-02-10 15:44:36 -0300

    fix: Removing parent unused dependencies

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
