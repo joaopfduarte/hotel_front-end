eb7d37f032fa0690e9fd49f2122c7f13dbd24821
agambrus <contato@ambrusplace.com.br>
2025-02-12 18:19:52 -0300

    fix: Reload added to Guest API

--- src/components/api/GuestApi.js
+++ src/components/api/GuestApi.js
@@ -26,21 +26,39 @@ class GuestApi extends BaseApi {
     const method = "POST";
     const url = `${this.baseUrl}api/v1/guest`;
     console.log(url);
-    return super.myFetch(null, method, url, guest);
+    return super
+      .myFetch(null, method, url, guest)
+      .then(() => {
+        window.location.reload();
+        alert('Hóspede incluído com sucesso!');
+      })
+      .catch((error) => alert(`Erro ao incluir hóspede: ${error.message}`));
   }
 
   alterarGuest(guest) {
     const method = "PUT";
     const url = `${this.baseUrl}api/v1/guest`;
     console.log(url);
-    return super.myFetch(null, method, url, guest);
+    return super
+      .myFetch(null, method, url, guest)
+      .then(() => {
+        window.location.reload();
+        alert('Hóspede alterado com sucesso!');
+      })
+      .catch((error) => alert(`Erro ao alterar hóspede: ${error.message}`));
   }
 
   excluir(id) {
     const method = "DELETE";
     const url = `${this.baseUrl}api/v1/guest/${id}`;
     console.log(url);
-    return super.myFetch(null, method, url);
+    return super
+      .myFetch(null, method, url)
+      .then(() => {
+        window.location.reload();
+        alert('Hóspede excluído com sucesso!');
+      })
+      .catch((error) => alert(`Erro ao excluir hóspede: ${error.message}`));
   }
 }
 
